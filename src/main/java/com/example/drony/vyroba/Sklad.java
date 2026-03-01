d2a6d1c0bea23b499cf2573c6cbdc06280427179
SomeKristi <kmaas2007@gmail.com>
2026-03-01 23:33:55 +0100

    Finished sklad

--- src/main/java/com/example/drony/vyroba/Sklad.java
+++ src/main/java/com/example/drony/vyroba/Sklad.java
@@ -1,23 +1,42 @@
 package com.example.drony.vyroba;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Sklad {
-    ArrayList<Objednavka> items = new ArrayList();
+    HashMap<String, Integer> items = new HashMap<>();
 
 
-    public synchronized void add(Objednavka[] itemToAdd) {
+    public synchronized boolean add(Objednavka[] itemToAdd) {
+        for (Objednavka item : itemToAdd) {
+            items.compute(
+                    item.getItem(),
+                    (key, value) ->
+                            value == null ? item.getAmount() : value + item.getAmount()
+            );
+        }
 
 
+        return true;
     }
 
     public synchronized boolean take(Objednavka[] itemsToTake) {
-//        check availible
+        for (Objednavka item : itemsToTake) {
+            if (items.get(item.getItem()) < 0) {
+                return false;
+            }
+        }
 
-//
+        for (Objednavka item : itemsToTake) {
+            items.compute(
+                    item.getItem(),
+                    (key, value) ->
+                            value == null ? 0 : value - item.getAmount()
+            );
+        }
 
-
+        return true;
     }
 
 
